58bf18f0b9b9f231f07c3a4508d9aa27771b29bf
Md-Akram <83151706+Md-Akram@users.noreply.github.com>
2021-10-16 18:01:08 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const hamburger = document.getElementById('hamburger')
 const mobileNav = document.getElementById('mobile-nav')
 
-console.log(mobileNav)
-
 hamburger.addEventListener('click', () => {
-  console.log(mobileNav.classList)
   mobileNav.classList.toggle('expand')
+
+  if (hamburger.attributes.src.value === 'images/icon-hamburger.svg') {
+    hamburger.attributes.src.value = 'images/icon-close.svg'
+  } else {
+    hamburger.attributes.src.value = 'images/icon-hamburger.svg'
+  }
 })
